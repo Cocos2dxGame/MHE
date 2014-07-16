dc117c9a888cce94fa2b09ebaca7328758e8b4b3
Zhao Yanxia <1716655706@qq.com>
2014-07-16 10:52:12 +0800

    参数修改为Layer*
    Signed-off-by: Zhao Yanxia <1716655706@qq.com>

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -92,7 +92,7 @@ bool GameScene::init()
 
 	//设置重力以及初始化BulletManager
 	Vec2 g(0, -800);
-	g_BulletManager = BulletManager::create((GameScene*)this, spritesVector, g);
+	g_BulletManager = BulletManager::create((Layer*)this, spritesVector, g);
 
 	this->scheduleUpdate();
 
